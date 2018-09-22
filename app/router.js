f2e84714879d14decc1257d729a518139cf7d9e8
Tiger Kong <tig567899@users.noreply.github.com>
2018-09-22 18:44:38 -0400

    added routes

--- app/router.js
+++ app/router.js
@@ -4,4 +4,131 @@ const sqlSearch = require('./sql-search');
 
 const router = express.Router();
 
+router.route('/login').post(async function(req, res)
+{
+    try
+	{
+		var data = await sqlSearch.login(req);
+		//console.log(data);
+		res.status(200).send(data);
+	}
+	catch(err)
+	{
+		next(err);
+		console.log(err);
+		res.status(500).send(data);
+		return;
+	}
+}
+
+router.route('/checkInfo').post(async function(req, res)
+{
+    try
+    {
+        var data = await sqlSearch.checkInfo(req);
+        //console.log(data);
+        res.status(200).send(data);
+    }
+    
+	catch(err)
+	{
+		next(err);
+		console.log(err);
+		res.status(500).send(data);
+		return;
+	}
+}
+
+router.route('/getData').post(async function(req, res)
+{
+    try
+    {
+        var data = await sqlSearch.getInfo(req);
+        //console.log(data);
+        res.status(200).send(data);
+    }
+    
+	catch(err)
+	{
+		next(err);
+		console.log(err);
+		res.status(500).send(data);
+		return;
+	}
+}
+
+router.route('/updateData').post(async function(req, res)
+{
+    try
+    {
+        var data = await sqlSearch.checkInfo(req);
+        //console.log(data);
+        res.status(200).send(data);
+    }
+    
+	catch(err)
+	{
+		next(err);
+		console.log(err);
+		res.status(500).send(data);
+		return;
+	}
+}
+
+router.route('/updateInfo').post(async function(req, res)
+{
+    try
+    {
+        var data = await sqlSearch.checkInfo(req);
+        //console.log(data);
+        res.status(200).send(data);
+    }
+    
+	catch(err)
+	{
+		next(err);
+		console.log(err);
+		res.status(500).send(data);
+		return;
+	}
+}
+
+router.route('/getInfo').post(async function(req, res)
+{
+    try
+    {
+        var data = await sqlSearch.checkInfo(req);
+        //console.log(data);
+        res.status(200).send(data);
+    }
+    
+	catch(err)
+	{
+		next(err);
+		console.log(err);
+		res.status(500).send(data);
+		return;
+	}
+}
+
+router.route('/makeOrder').post(async function(req, res)
+{
+    try
+    {
+        var data = await sqlSearch.checkInfo(req);
+        //console.log(data);
+        res.status(200).send(data);
+    }
+    
+	catch(err)
+	{
+		next(err);
+		console.log(err);
+		res.status(500).send(data);
+		return;
+	}
+}
+
 module.exports = router;
+
+// 1 login 2 check info 3 retrieve info 4 send info 5 modify info 6 chapter info 7 create order to a clinic

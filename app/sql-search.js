cc183fe47ee35f0dc6ecc9f9d7531b029e22277f
Tiger Kong <tig567899@users.noreply.github.com>
2018-09-22 20:11:59 -0400

    deleted checkinfo

--- app/sql-search.js
+++ app/sql-search.js
@@ -7,6 +7,17 @@ connect({
     database: 'yorkhacks'
 });
 
+async function login(req)
+{
+    var username = req.body.username;
+    var pass = req.body.password;
+    var sql = "SELECT password FROM login WHERE username = ?";
+    
+    const result = await query(sql, [username]);
+    
+    if
 
+    return { code: 200, message: "Logged in" }
+}
 
-module.exports = {login, checkInfo, getInfo, getData, updateData, updateInfo, makeOrder};
+module.exports = {login, getInfo, getData, updateData, updateInfo, makeOrder};

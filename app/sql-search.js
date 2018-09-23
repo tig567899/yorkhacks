e4be5e18c5429c3361c1aaef056dff26bfadeeda
Belgua Whale <belguawhale@gmail.com>
2018-09-23 10:47:55 -0400

    fix json parse shitff

--- app/sql-search.js
+++ app/sql-search.js
@@ -41,7 +41,7 @@ async function getData(req) {
     else
     {
         var sql = "SELECT supplies, name FROM data WHERE name = ?";
-        result = JSON.parse(await query(sql, [username]));
+        result = await query(sql, [username]);
     }
     
     var array = {
@@ -58,17 +58,18 @@ async function getData(req) {
         antiseptics: 0};
     for (var x = 0; x < result.length; x++)
     {
-        array.bandages += (result[x].supplies.bandages == null ? 0 : result[x].supplies.bandages);
-        array.examination_supplies += (result[x].supplies.examination_supplies == null ? 0 : result[x].supplies.bandages);
-        array.scalpels += (result[x].supplies.scalpels == null ? 0 : result[x].supplies.bandages);
-        array.iv_kits += (result[x].supplies.array == null ? 0 : result[x].supplies.bandages);
-        array.masks += (result[x].supplies.masks == null ? 0 : result[x].supplies.bandages);
-        array.needles += (result[x].supplies.needles == null ? 0 : result[x].supplies.bandages);
-        array.vitamins += (result[x].supplies.vitamins == null ? 0 : result[x].supplies.bandages);
-        array.dental_supplies += (result[x].supplies.dental_supplies == null ? 0 : result[x].supplies.bandages);
-        array.optical_supplies += (result[x].supplies.optical_supplies == null ? 0 : result[x].supplies.bandages);
-        array.personal_hygiene += (result[x].supplies.personal_hygiene == null ? 0 : result[x].supplies.bandages);
-        array.antiseptics += (result[x].supplies.personal_hygiene == null ? 0 : result[x].supplies.bandages);
+        supplies = JSON.parse(result[x].supplies)
+        array.bandages += (supplies.bandages == null ? 0 : supplies.bandages);
+        array.examination_supplies += (supplies.examination_supplies == null ? 0 : supplies.bandages);
+        array.scalpels += (supplies.scalpels == null ? 0 : supplies.bandages);
+        array.iv_kits += (supplies.array == null ? 0 : supplies.bandages);
+        array.masks += (supplies.masks == null ? 0 : supplies.bandages);
+        array.needles += (supplies.needles == null ? 0 : supplies.bandages);
+        array.vitamins += (supplies.vitamins == null ? 0 : supplies.bandages);
+        array.dental_supplies += (supplies.dental_supplies == null ? 0 : supplies.bandages);
+        array.optical_supplies += (supplies.optical_supplies == null ? 0 : supplies.bandages);
+        array.personal_hygiene += (supplies.personal_hygiene == null ? 0 : supplies.bandages);
+        array.antiseptics += (supplies.personal_hygiene == null ? 0 : supplies.bandages);
     }
     console.log(result[0].supplies);
     return { code: 200, message: array };

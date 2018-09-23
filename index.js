55c78c5f91443b181218fccd736d26ede899c98f
Tiger Kong <tig567899@users.noreply.github.com>
2018-09-23 00:55:10 -0400

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const apiRouter = require('./app/router');
@@ -7,26 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/yhacksapi', apiRouter);
 // Add headers
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 app.listen(PORT, () => { console.log(`Server listening on http://localhost:${PORT}`) });
